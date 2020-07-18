bab22b2525d3e4004e9199174b6c715baad016ea
Razvan Musaloiu-E <razvanm@google.com>
2020-07-18 04:59:47 +0000

    Add an initial CircleCI configuration

--- web/e2e/src/app.e2e-spec.ts
+++ web/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('web app is running!');
+    expect(page.getTitleText()).toEqual('my-pushes works!');
   });
 
   afterEach(async () => {
