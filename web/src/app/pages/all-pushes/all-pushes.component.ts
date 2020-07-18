5be2a622603d3a5438a973bfecb45acb761c2902
Razvan Musaloiu-E <razvanm@google.com>
2020-07-18 03:25:08 +0000

    Add components for the 3 main pages
    The 3 components were created using the following commands:
    
        ng generate component pages/my-pushes
        ng generate component pages/all-pushes
        ng generate component pages/one-push
    
    This PR also removes the placeholder content from the HTML for
    AppComponent.

---
+++ web/src/app/pages/all-pushes/all-pushes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-pushes',
+  templateUrl: './all-pushes.component.html',
+  styleUrls: ['./all-pushes.component.scss']
+})
+export class AllPushesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ web/src/app/pages/my-pushes/my-pushes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-pushes',
+  templateUrl: './my-pushes.component.html',
+  styleUrls: ['./my-pushes.component.scss']
+})
+export class MyPushesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
