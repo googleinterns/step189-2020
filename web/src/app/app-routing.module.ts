16d923c5f0f33bac295bad49f06dd34da191671f
Razvan Musaloiu-E <razvanm@google.com>
2020-07-22 16:06:18 -0700

    Update the AppRoutingModule to route to our pages (#4)
    The AppRoutingModule is changed to route our pages in the following way:
    
      - use the MyPushesComponent for the root page
      - use the OnePushComponent for the URLs that contains '/@'
      - use the AllPushesComponent for all the other pages.
    
    This PR also populates the HTML for the components for the pages with
    some temporary links to demonstrate the routing.

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, UrlSegment, UrlMatchResult } from '@angular/router';
 
-const routes: Routes = [];
+import { AllPushesComponent } from './pages/all-pushes/all-pushes.component';
+import { MyPushesComponent } from './pages/my-pushes/my-pushes.component';
+import { OnePushComponent } from './pages/one-push/one-push.component';
+
+/**
+ * Matcher for the URLs for individual pushes.
+ *
+ * Example: /test/razvanm/helloworld/@20180503-163004.520847
+ */
+export function onePushMatcher(segments: UrlSegment[]): UrlMatchResult {
+  const noMatch = {consumed: []};
+  if (!segments[segments.length - 1].path.startsWith('@')) {
+    return noMatch;
+  }
+  return {consumed: segments};
+}
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MyPushesComponent,
+    pathMatch: 'full',
+  },
+  {
+    component: OnePushComponent,
+    matcher: onePushMatcher,
+  },
+  {
+    path: '**',
+    component: AllPushesComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
