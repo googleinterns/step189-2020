7ed081938a90775dd5e9178e7c03e8da9b1196cc
Razvan Musaloiu-E <razvanm@google.com>
2020-08-06 17:21:55 -0700

    Add support for Protocol Buffers and some sample data (#13)
    This PR adds a proto definition for the data we are going to work with.
    The protobuf.js [1] library is used to generate the TypeScript code from
    the .proto file. The advantages of using protobuf.js instead of the
    compiler provided by Google [2] is that:
    
      (a) it provides support for TypeScript
      (b) it comes readily available via npm
      (b) it provides a superior parsing performance.
    
    The protobuf.js can generate static code, use a JSON representation of
    the .proto files or use the .proto files directly. To keep things simple
    this PR is relying on the static code. The steps for generating the code
    can be run using `npm run genproto`.
    
    This PR also adds an initial set of sample data: each .pb from the
    src/assets/ contains a single PushInfos message.
    
    [1] https://github.com/protobufjs/protobuf.js
    [2] https://github.com/protocolbuffers/protobuf

--- web/src/app/pages/all-pushes/all-pushes.component.ts
+++ web/src/app/pages/all-pushes/all-pushes.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { flatMap, map, shareReplay } from 'rxjs/operators';
+
+import { step189_2020 } from '../../../proto/step189_2020';
 
 @Component({
   selector: 'app-all-pushes',
   templateUrl: './all-pushes.component.html',
   styleUrls: ['./all-pushes.component.scss']
 })
-export class AllPushesComponent implements OnInit {
+export class AllPushesComponent {
+  readonly pushDefName: Observable<string>;
+  readonly pushInfos: Observable<step189_2020.IPushInfo[]>;
 
-  constructor() { }
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly http: HttpClient) {
+      this.pushDefName = this.route.url.pipe(
+        map((urlSegments) => {
+          return urlSegments.map(urlSegment => urlSegment.path).join('/');
+        }),
+        shareReplay(1));
 
-  ngOnInit(): void {
-  }
-
+      this.pushInfos = this.pushDefName.pipe(flatMap(pushDefName => {
+        return this.http.get(`assets/${pushDefName}.pb`, {responseType: 'arraybuffer'})
+          .pipe(map((data: ArrayBuffer) => {
+            const pushInfos = step189_2020.PushInfos.decode(new Uint8Array(data));
+            console.log(pushInfos);
+            return pushInfos.pushInfo;
+          }));
+      }),
+      shareReplay(1));
+    }
 }
