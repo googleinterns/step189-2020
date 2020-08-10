4576761fa3ea0a68e30e67b03eb118ff48e8804a
Razvan Musaloiu-E <razvanm@google.com>
2020-08-10 09:16:27 -0700

    Populate the one-push page (#19)
    This PR populates the one-push page with the protos added in #13.

--- web/src/app/pages/one-push/one-push.component.ts
+++ web/src/app/pages/one-push/one-push.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
+import { flatMap, map, shareReplay } from 'rxjs/operators';
+import { formatDate } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Observable, combineLatest } from 'rxjs';
+
+import { step189_2020 } from '../../../proto/step189_2020';
 
 @Component({
   selector: 'app-one-push',
   templateUrl: './one-push.component.html',
   styleUrls: ['./one-push.component.scss']
 })
-export class OnePushComponent implements OnInit {
+export class OnePushComponent {
+  readonly pushHandle: Observable<string>;
+  readonly pushDefName: Observable<string>;
+  readonly pushInfos: Observable<step189_2020.IPushInfo[]>;
+  readonly pushInfo: Observable<step189_2020.IPushInfo>;
 
-  constructor() { }
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly http: HttpClient) {
+      this.pushHandle = this.route.url.pipe(
+        map((urlSegments) => {
+          return urlSegments.map(urlSegment => urlSegment.path).join('/');
+        }),
+        shareReplay(1));
 
-  ngOnInit(): void {
+      this.pushDefName = this.pushHandle.pipe(
+        map(pushHandle => pushHandle.split('/@')[0]),
+        shareReplay(1));
+
+      this.pushInfos = this.pushDefName.pipe(flatMap(pushDefName => {
+        return this.http.get(`assets/${pushDefName}.pb`, {responseType: 'arraybuffer'})
+          .pipe(map((data: ArrayBuffer) => {
+            const pushInfos = step189_2020.PushInfos.decode(new Uint8Array(data));
+            console.log(pushInfos);
+            return pushInfos.pushInfo;
+          }));
+        }),
+        shareReplay(1));
+
+      this.pushInfo = combineLatest([this.pushHandle, this.pushInfos]).pipe(
+        map(([pushHandle, pushInfos]) => {
+          return pushInfos.filter(pushInfo => pushInfo.pushHandle === pushHandle)[0];
+        }),
+        shareReplay(1));
   }
 
+  formatTimeNsec(ts: number | Long | null | undefined): string {
+    if (typeof ts === 'number') {
+      return formatDate(ts / 1000 / 1000, 'yyyy-MM-dd HH:mm:ss.SSS zzzz', 'en-US');
+    }
+    return '';
+  }
 }
