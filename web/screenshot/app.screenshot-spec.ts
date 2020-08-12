f58b1a7335c9be6af4e6f69d1e2b66b71cb31587
Carol Sun <cksun@google.com>
2020-08-12 16:46:44 +0000

    Delete app.po.ts and remove beforeeach

--- web/screenshot/app.screenshot-spec.ts
+++ web/screenshot/app.screenshot-spec.ts
@@ -1,11 +1,8 @@
-import { AppPage } from './app.po';
 import { browser} from 'protractor';
 
 describe('protractor-image-comparison desktop', () => {
   it('should be the same as the baseline', async () => {
-    beforeEach(async () => {
-      await browser.get(browser.baseUrl);
-    });
+    await browser.get(browser.baseUrl);
     expect(await browser.imageComparison.checkScreen('my-pushes')).toEqual(0);
   });
 });
