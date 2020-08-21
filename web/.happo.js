637673f14168aa972e1086dcb7e16ce390982e06
Carol Sun <44757906+carolksun@users.noreply.github.com>
2020-08-21 09:45:28 -0700

    try string concatenation

--- web/.happo.js
+++ web/.happo.js
@@ -7,12 +7,12 @@ module.exports = {
   
   pages: [
     { url: process.env.URL, title: 'my-pushes' },
-    { url: process.env.URL/28a1555e453f, title: 'all-pushes 11k' },
-    { url: process.env.URL/28a1555e453f/@20200805-211425.620604, title: 'built unchanged one-push 11k' },
-    { url: process.env.URL/28a1555e453f/@20200724-212811.116442, title: 'completed one-push 11k' },
-    { url: process.env.URL/7f4535707267, title: 'all-pushes 83' },
-    { url: process.env.URL/7f4535707267/@20200413-170000.042638, title: 'completed one push 83' },
-    { url: process.env.URL/7f4535707267/@20200316-170000.080599, title: 'reverted one push 83' },
+    { url: process.env.URL + "/28a1555e453f", title: 'all-pushes 11k' },
+    //{ url: process.env.URL/28a1555e453f/@20200805-211425.620604, title: 'built unchanged one-push 11k' },
+    //{ url: process.env.URL/28a1555e453f/@20200724-212811.116442, title: 'completed one-push 11k' },
+    //{ url: process.env.URL/7f4535707267, title: 'all-pushes 83' },
+    //{ url: process.env.URL/7f4535707267/@20200413-170000.042638, title: 'completed one push 83' },
+    //{ url: process.env.URL/7f4535707267/@20200316-170000.080599, title: 'reverted one push 83' },
   ],
 
   targets: {
