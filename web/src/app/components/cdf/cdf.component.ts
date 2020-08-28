11a63874cce55e7518d3e31a52535640f0ad3a40
Carol Sun <cksun@google.com>
2020-08-28 19:16:56 +0000

    rebase on main

--- web/src/app/components/cdf/cdf.component.ts
+++ web/src/app/components/cdf/cdf.component.ts
@@ -3,7 +3,7 @@ import * as d3 from 'd3';
 
 import {step189_2020} from '../../../proto/step189_2020';
 
-import {addCurrentPushLine, generateQuantiles, generateYPosition, populateData} from './cdf.utils';
+import {addCurrentPushLine, getProbabilityForDuration, generateQuantiles, generateYPosition, populateData} from './cdf.utils';
 import {COMPLETED_BLUE, d3SVG, Item, STROKE_COLOR} from './cdf.utils';
 
 @Component({
@@ -92,7 +92,8 @@ export class CDFComponent implements AfterViewInit {
     const extendedData = Array.from(this.data);
     extendedData.push(
         {duration: xScale.ticks()[xScale.ticks().length - 1], probability: 1});
-
+    
+    const maxExtendedDuration = extendedData[extendedData.length - 1].duration;
     this.svg = (d3.select(element).append('svg') as d3SVG)
                    .attr('width', elementWidth)
                    .attr('height', elementHeight);
@@ -245,5 +246,220 @@ export class CDFComponent implements AfterViewInit {
 
     addCurrentPushLine(
         this.currentPush, currentPushLine, this.data, height, xScale, yScale);
+
+    // Mouse click
+    let startValue = minDuration + 1;
+
+    const clipRect = cdfChart
+      .append('clipPath')
+      .attr('id', 'area-clip')
+      .append('rect')
+      .attr('class', 'area-clip-rect')
+      .attr('x', 0)
+      .attr('y', 0)
+      .attr('width', xScale(startValue))
+      .attr('height', height);
+
+    cdfChart
+      .datum(extendedData)
+      .append('path')
+      .attr('class', 'cdf-curve')
+      .attr('d', d3.area<Item>()
+        .x(d => xScale(d.duration))
+        .y1(d => yScale(d.probability))
+        .y0(yScale(0))
+        .curve(d3.curveStepAfter)
+      )
+      .attr('fill-opacity', '0.6')
+      .attr('fill', 'white')
+      .attr('clip-path', 'url(#area-clip)');
+
+    const lineY = cdfChart
+      .append('line')
+      .attr('class', 'click-line-y')
+      .attr('x1', xScale(startValue))
+      .attr('x2', xScale(startValue))
+      .attr('y1', yScale(getProbabilityForDuration(this.data, startValue)))
+      .attr('y2', height)
+      .attr('stroke', 'black')
+      .attr('stroke-width', '1px')
+      .attr('opacity', 0);
+
+    const lineX = cdfChart
+      .append('line')
+      .attr('class', 'click-line-x')
+      .attr('x1', 0)
+      .attr('x2', xScale(startValue))
+      .attr('y1', yScale(getProbabilityForDuration(this.data, startValue)))
+      .attr('y2', yScale(getProbabilityForDuration(this.data, startValue)))
+      .attr('stroke', 'black')
+      .attr('stroke-width', '1px')
+      .attr('opacity', 0);
+
+    cdfChart
+      .append('text')
+      .attr('x', xScale(startValue) - 35)
+      .attr('y', yScale(getProbabilityForDuration(this.data, startValue) / 100) + 30)
+      .attr('class', 'click-line-y-text')
+      .attr('font-size', '10px')
+      .text(`${this.data.filter(c => c.duration <= startValue).length}/${this.data.length}`)
+      .attr('opacity', 0);
+
+    cdfChart
+      .append('text')
+      .attr('x', xScale(startValue) / 2)
+      .attr('y', yScale(getProbabilityForDuration(this.data, startValue) / 100) + 10)
+      .attr('class', 'click-line-x-text')
+      .attr('font-size', '10px')
+      .text(`${getProbabilityForDuration(this.data, startValue).toFixed(2)}%`)
+      .attr('opacity', 0);
+
+    cdfChart.on('click', (d: unknown, i: number): void => {
+      if (!d) { return; }
+      const coordinates = d3.mouse(d3.event.currentTarget);
+      const xValue = xScale.invert(coordinates[0]);
+      if (xValue > minDuration) {
+        startValue = xValue;
+        const yValue = getProbabilityForDuration(d as Item[], startValue);
+        d3.select(d3.event.currentTarget)
+          .select('.area-clip-rect')
+          .attr('width', xScale(startValue));
+        d3.select(d3.event.currentTarget)
+          .select('.click-line-y')
+          .attr('y1', yScale(yValue))
+          .attr('x1', xScale(startValue))
+          .attr('x2', xScale(startValue))
+          .attr('opacity', 1);
+        d3.select(d3.event.currentTarget)
+          .select('.click-line-x')
+          .attr('y1', yScale(yValue))
+          .attr('y2', yScale(yValue))
+          .attr('x2', xScale(startValue))
+          .attr('opacity', 1);
+        d3.select(d3.event.currentTarget)
+          .select('.click-line-y-text')
+          .attr('x', xScale(startValue) - 40)
+          .attr('y', yScale(yValue) + 20)
+          .text(`${this.data.filter(c => c.duration <= startValue).length}/${this.data.length}`)
+          .attr('opacity', 1);
+        d3.select(d3.event.currentTarget)
+          .select('.click-line-x-text')
+          .attr('x', xScale(startValue) / 2)
+          .attr('y', yScale(yValue) + 10)
+          .text(`${(getProbabilityForDuration(this.data, startValue) * 100).toFixed(1)}%`)
+          .attr('opacity', 1);
+      }
+    });
+
+    // hover
+    const highlightColor = '#b9edc4';
+    const strokeColor = '#167364';
+    const circleColor = '#a0aade';
+    const labelsize = [40, 25];
+    const labelFontSize = labelsize[1] / 2;
+
+    cdfChart.append('rect')
+      .attr('class', 'x-label-bg')
+      .attr('x', 0)
+      .attr('y', height)
+      .attr('height', labelsize[1])
+      .attr('width', labelsize[0])
+      .attr('fill', highlightColor)
+      .attr('opacity', 0);
+
+    cdfChart.append('text')
+      .attr('text-anchor', 'middle')
+      .attr('class', 'y-label')
+      .attr('x', -labelsize[0] / 2)
+      .attr('y', 0)
+      .attr('opacity', 0)
+      .attr('fill', 'black')
+      .style('font-size', `${labelFontSize}px`);
+
+    cdfChart.append('line')
+      .attr('x1', 0)
+      .attr('x2', 0)
+      .attr('y1', height)
+      .attr('y2', 0)
+      .attr('class', 'v-ruler')
+      .attr('stroke', 'grey')
+      .attr('stroke-width', 2)
+      .attr('stroke-dasharray', '5,2')
+      .attr('opacity', 0);
+
+    cdfChart.append('line')
+      .attr('x1', 0)
+      .attr('x2', width)
+      .attr('y1', 0)
+      .attr('y2', 0)
+      .attr('class', 'h-ruler')
+      .attr('stroke', 'grey')
+      .attr('stroke-width', 2)
+      .attr('stroke-dasharray', '5,2')
+      .attr('opacity', 0);
+
+    cdfChart.append('circle')
+      .attr('class', 'marker')
+      .attr('cx', 0)
+      .attr('cy', 0)
+      .attr('r', 5)
+      .attr('stroke', strokeColor)
+      .attr('stroke-width', 2)
+      .attr('fill', circleColor)
+      .style('opacity', 0);
+
+    cdfChart.on('mouseover', (d: unknown, i: number): void => {
+      const mouseX = xScale.invert(d3.mouse(d3.event.currentTarget)[0]);
+      const vruler = d3.select('.v-ruler');
+      const hruler = d3.select('.h-ruler');
+      const marker = d3.select('.marker');
+      const xlabel = d3.select('.x-label');
+      const xlabelbg = d3.select('.x-label-bg');
+      const ylabel = d3.select('.y-label');
+      const ylabelbg = d3.select('.y-label-bg');
+      const checkX = (mouseX >= minDuration) && (mouseX <= maxExtendedDuration);
+
+      if (checkX) {
+        const xVal = d3.mouse(d3.event.currentTarget)[0];
+        const yVal = yScale(getProbabilityForDuration(d as Item[], xScale.invert(xVal)));
+
+        marker.attr('cx', xVal).attr('cy', yVal);
+        hruler.attr('y1', yVal).attr('y2', yVal);
+        vruler.attr('x1', xVal).attr('x2', xVal);
+
+        const xText = d3.format(',.1f')(xScale.invert(+marker.attr('cx')));
+        const yText = d3.format(',.1%')(yScale.invert(+marker.attr('cy')));
+        xlabel.attr('x', +marker.attr('cx')).text(xText);
+        xlabelbg.attr('x', +marker.attr('cx') - labelsize[0] / 2);
+        ylabel.attr('y', +marker.attr('cy') + labelsize[1] / 5).text(yText);
+        ylabelbg.attr('y', +marker.attr('cy') - labelsize[1] / 2);
+
+        marker.style('opacity', 1);
+        hruler.style('opacity', 1);
+        vruler.style('opacity', 1);
+        xlabel.style('opacity', 1);
+        xlabelbg.style('opacity', 1);
+        ylabel.style('opacity', 1);
+        ylabelbg.style('opacity', 1);
+      } else {
+        marker.style('opacity', 0);
+        hruler.style('opacity', 0);
+        vruler.style('opacity', 0);
+        xlabel.style('opacity', 0);
+        xlabelbg.style('opacity', 0);
+        ylabel.style('opacity', 0);
+        ylabelbg.style('opacity', 0);
+      }
+    });
+
+    cdfChart.on('mouseleave', (d: unknown, i: number): void => {
+      const vruler = d3.select('.v-ruler').style('opacity', 0);
+      const hruler = d3.select('.h-ruler').style('opacity', 0);
+      const marker = d3.select('.marker').style('opacity', 0);
+      const xlabel = d3.select('.x-label').style('opacity', 0);
+      const xlabelbg = d3.select('.x-label-bg').style('opacity', 0);
+      const ylabel = d3.select('.y-label').style('opacity', 0);
+      const ylabelbg = d3.select('.y-label-bg').style('opacity', 0);
+    });
   }
 }
