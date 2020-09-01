569a3ac0c2bb0ea012e757517beb13decef72810
Razvan Musaloiu-E <razvanm@google.com>
2020-08-31 18:15:52 -0700

    Add the Apache 2.0 license headers (#40)
    This PR doesn't touch the empty files and also the .html files.

--- web/src/app/components/cdf/cdf.utils.ts
+++ web/src/app/components/cdf/cdf.utils.ts
@@ -1,7 +1,23 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import * as d3 from 'd3';
 
 import {step189_2020} from '../../../proto/step189_2020';
-import {DurationItem, findDurationUnit, findDuration, UNIT_CONVERSION} from '../duration.utils';
+import {DurationItem, findDuration, findDurationUnit, UNIT_CONVERSION} from '../duration.utils';
 
 export interface Item {
   duration: number;     // Time between last stage and first non-empty stage
